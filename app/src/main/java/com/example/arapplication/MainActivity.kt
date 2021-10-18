1185ea2cdb9475ca7dea59cb45a7c0b695ee4122
JunghunKum <39320407+kumgo1d@users.noreply.github.com>
2021-10-18 23:29:26 +0900

    add hilt
     - hilt 공부 및 적용중

--- app/src/main/java/com/example/arapplication/MainActivity.kt
+++ app/src/main/java/com/example/arapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.arapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.arapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/arapplication/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.arapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arapplication.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
